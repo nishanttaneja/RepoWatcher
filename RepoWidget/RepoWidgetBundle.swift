65447d3f5e0c4c24e9cdcfab7d57f257bce4fd47
nishant-ios <nishant.iosdev@gmail.com>
2025-03-17 23:36:22 +0530

    initial commit

---
+++ RepoWidget/RepoWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  RepoWidgetBundle.swift
+//  RepoWidget
+//
+//  Created by Nishant Taneja on 17/03/25.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct RepoWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        RepoWidget()
+    }
+}
