9f7473148e321971b1b72405a6c22bbdb560bef4
nishant-ios <nishant.iosdev@gmail.com>
2025-03-17 23:42:17 +0530

    adding RepoContributorWidget

---
+++ RepoContributorWidget/RepoContributorWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  RepoContributorWidgetBundle.swift
+//  RepoContributorWidget
+//
+//  Created by Nishant Taneja on 17/03/25.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct RepoContributorWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        RepoContributorWidget()
+    }
+}
