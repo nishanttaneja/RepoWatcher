3537be529d9ecace1a47dcb5b565288053c96c6f
nishant-ios <nishant.iosdev@gmail.com>
2025-03-19 00:56:10 +0530

    combining widgets into single widget

--- RepoWatcherWidget/RepoDetailsWidget.swift
+++ RepoWatcherWidget/RepoDetailsWidget.swift
@@ -14,8 +14,15 @@ fileprivate struct RepoDetailsEntry: TimelineEntry {
 }
 
 fileprivate extension RepoDetailsEntry {
-    static let placeholderData = RepoDetailsEntry(date: .now, details: RepoDetails(ownerImagePath: "", title: "Hello, GitHub", description: "There's no description available for this repository.", daysSinceLastActivity: 5, watchers: 9, forks: 3, issues: 2, contributors: []))
-    static let mockData = RepoDetailsEntry(date: .now, details: RepoDetails(ownerImagePath: "", title: "Real-Dicee", description: "An iOS Application using ARKit and SCNKit to display Dice in Real World.", daysSinceLastActivity: 1648, watchers: 2, forks: 0, issues: 0, contributors: []))
+    static let placeholderData = RepoDetailsEntry(date: .now, details: RepoDetails(ownerImagePath: "", title: "Hello, GitHub", description: "There's no description available for this repository.", daysSinceLastActivity: 5, watchers: 9, forks: 3, issues: 2, contributors: [
+        RepoDetails.Contributor(userImagePath: "", username: "username", contributions: 6),
+        RepoDetails.Contributor(userImagePath: "", username: "username", contributions: 5),
+        RepoDetails.Contributor(userImagePath: "", username: "username", contributions: 4),
+        RepoDetails.Contributor(userImagePath: "", username: "username", contributions: 3)
+    ]))
+    static let mockData = RepoDetailsEntry(date: .now, details: RepoDetails(ownerImagePath: "", title: "Real-Dicee", description: "An iOS Application using ARKit and SCNKit to display Dice in Real World.", daysSinceLastActivity: 1648, watchers: 2, forks: 0, issues: 0, contributors: [
+        RepoDetails.Contributor(userImagePath: "", username: "nishanttaneja", contributions: 10)
+    ]))
 }
 
 fileprivate struct RepoDetailsProvider: TimelineProvider {
@@ -31,9 +38,11 @@ fileprivate struct RepoDetailsProvider: TimelineProvider {
 
     func getTimeline(in context: Context, completion: @escaping @Sendable (Timeline<RepoDetailsEntry>) -> Void) {
         Task {
-            let repoDetails: RepoDetails
+            var repoDetails: RepoDetails
             do {
                 repoDetails = try await URLSession.getRepoDetails() ?? .mockData
+                let contributors = try await URLSession.getRepoContributors()
+                repoDetails.setContributors(contributors)
             } catch {
                 debugPrint(#function, error)
                 repoDetails = .mockData
@@ -44,7 +53,7 @@ fileprivate struct RepoDetailsProvider: TimelineProvider {
     }
 }
 
-struct RepoDetailsView: View {
+fileprivate struct RepoDetailsView: View {
     private let details: RepoDetails
     
     init(_ details: RepoDetails) {
@@ -128,20 +137,99 @@ struct RepoDetailsView: View {
     }
 }
 
-struct RepoDetailsWidget: Widget {
+fileprivate struct RepoContributorsView: View {
+    private let details: RepoDetails
+    
+    init(_ details: RepoDetails) {
+        self.details = details
+    }
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+            RepoDetailsView(details)        // Repo Details
+            Spacer().frame(height: 8)
+            Rectangle()
+                .fill(.thinMaterial)
+                .frame(height: 1)
+            if details.contributors.count > .zero {
+                Spacer().frame(height: 8)
+                Text("Top Contributors")         // Contributors List View
+                    .font(.caption)
+                    .fontWeight(.semibold)
+                    .foregroundStyle(.secondary)
+                LazyVGrid(columns: Array(repeating: GridItem(.flexible()), count: 2), alignment: .leading, spacing: 8) {
+                    ForEach(details.contributors) { contributor in
+                            HStack {        // Image and title
+                                Image(contentsOf: contributor.userImagePath, placeholderImage: .avatar)
+                                    .resizable()
+                                    .widgetAccentedRenderingMode(.desaturated)
+                                    .frame(width: 36, height: 36)
+                                    .clipShape(Circle())
+                                VStack(alignment: .leading) {
+                                    Text(contributor.username)
+                                        .font(.caption)
+                                        .minimumScaleFactor(0.8)
+                                        .lineLimit(1)
+                                        .widgetAccentable()
+                                        .contentTransition(.opacity)
+                                    Text("\(contributor.contributions)")
+                                        .font(.caption)
+                                        .fontWeight(.light)
+                                        .foregroundStyle(.secondary)
+                                        .minimumScaleFactor(0.7)
+                                        .contentTransition(.numericText())
+                                }
+                            }
+                    }
+                }
+            }
+            Spacer()
+        }
+        .containerBackground(for: .widget) { }
+    }
+}
+
+fileprivate struct RepoDetailsWidgetView: View {
+    @Environment(\.widgetFamily) private var widgetFamily
+    private let details: RepoDetails
+    
+    init(_ details: RepoDetails) {
+        self.details = details
+    }
+    
+    var body: some View {
+        switch widgetFamily {
+        case .systemMedium:
+            RepoDetailsView(details)
+        case .systemLarge:
+            RepoContributorsView(details)
+        default:
+            Text("unsupported widget family")
+        }
+    }
+}
+
+@main
+fileprivate struct RepoDetailsWidget: Widget {
     private let kind = "RepoDetailsWidget"
     
     var body: some WidgetConfiguration {
         StaticConfiguration(kind: kind, provider: RepoDetailsProvider()) { entry in
-            RepoDetailsView(entry.details)
+            RepoDetailsWidgetView(entry.details)
         }
         .configurationDisplayName("Repo Watcher")
         .description("Keep an eye on a GitHub repository.")
-        .supportedFamilies([.systemMedium])
+        .supportedFamilies([.systemMedium, .systemLarge])
     }
 }
 
 #Preview(as: .systemMedium, widget: {
+    RepoDetailsWidget()
+}, timeline: {
+    RepoDetailsEntry.placeholderData
+    RepoDetailsEntry.mockData
+})
+#Preview(as: .systemLarge, widget: {
     RepoDetailsWidget()
 }, timeline: {
     RepoDetailsEntry.placeholderData
